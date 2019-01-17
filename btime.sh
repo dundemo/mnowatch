25af4975dc854056b596183cc3934ba2a7322995
dundemo <46777105+dundemo@users.noreply.github.com>
2019-01-17 10:41:37 +0200

    Add files via upload

---
+++ btime.sh
@@ -0,0 +1,7 @@
+#Please put THIS_IS_CRON=1 in crotab
+if [ -n "$THIS_IS_CRON" ]
+then 
+btime=$(($(dash-cli getblockcount)-880648-1662))&&remainder=$(($btime % 16616));if [ $remainder -le 10 ];then /home/demo/bin/mnowatch.sh -super;fi
+else
+echo "I'm running only in cron"
+fi
