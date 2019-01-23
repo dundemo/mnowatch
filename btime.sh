fb8a366456f4cb510b393ec050247bc5702ed4a9
dundemo <demo@copernicus5>
2019-01-24 08:02:35 +1100

    demo

--- btime.sh
+++ btime.sh
@@ -7,13 +7,23 @@
 # MNOWATCH VERSION: 0.02
 
 #=============== ISTRUCTIONS ==========================================
-# In order for this to work you have to put THIS_IS_CRON=1 into crontab
-# Look at crontab.example
+# 1) SET MYHOME_DIR.
+MYHOME_DIR="/home/demo"
+# 2) ALL MNOWATCH GITHUB FILES MUST RESIDE INTO $MYHOME_DIR/bin
+# 3) In order for this to work you have to put THIS_IS_CRON=1 into crontab.
+#    Look at the provided crontab.example file.
 #=============== END OF ISTRUCTIONS ===================================
 
 if [ -n "$THIS_IS_CRON" ]
 then 
-btime=$(($(dash-cli getblockcount)-880648-1662))&&remainder=$(($btime % 16616));if [ $remainder -le 10 ];then /home/demo/bin/mnowatch.sh -super;fi
+ btime=$(($(dash-cli getblockcount)-880648-1662))&&remainder=$(($btime % 16616))
+ if [ $remainder -le 10 ]
+ then 
+  echo $btime","$remainder","`date -u` > /tmp/The_voting_deadline
+  $MYHOME_DIR/bin/mnowatch.sh -super
+ else
+  echo $btime","$remainder","`date -u` > /tmp/Not_a_voting_deadline
+ fi
 else
-echo "I'm running only in cron"
+ echo "This bash should be executed only by cron daemon."
 fi
