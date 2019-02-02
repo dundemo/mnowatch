b3d257202fb57b15c27763a2138693736786a0ad
Michael <xkcd@mpx.com.au>
2019-02-02 17:32:38 +1100

    Rewrite/refactor and increase frequencey of running mnowatch
    mnowatch will now run hourly on the final day of voting and will run immediately after the voting closes.  Lots of refactoring in the rewrite should be easier to extend it in the future if required.  Added additional sanity checks to abort this program if the environment is not correct.

--- btime.sh
+++ btime.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+#set -x
 
 # Licence: GPLv2
 # The author of the software is the owner of the Dash Address: XnpT2YQaYpyh7F9twM6EtDMn1TCDCEEgNX
-# Tweaking / Debugging / Blockchain Advices by xkcd@dashtalk
+# Tweaking / Debugging / Blockchain Advices by xkcd#7307@dashtalk Discord
 #
-# MNOWATCH VERSION: 0.04
+# MNOWATCH VERSION: 0.05
 
-#=============== ISTRUCTIONS ==========================================
+#=============== INSTRUCTIONS =========================================
 # 1) SET MYHOME_DIR.
 MYHOME_DIR="/home/demo"
 # 2) ALL MNOWATCH GITHUB FILES MUST RESIDE INTO $MYHOME_DIR/bin
@@ -14,17 +15,33 @@ MYHOME_DIR="/home/demo"
 #    Look at the provided crontab.example file.
 #=============== END OF ISTRUCTIONS ===================================
 
-if [ -n "$THIS_IS_CRON" ]
-then 
- btime=$(($(dash-cli getblockcount)-880648+1662))&&remainder=$(($btime % 16616))
- if [ $remainder -le 10 ]
- then 
-  echo $btime","$remainder","`date -u` > /tmp/The_voting_deadline
-  $MYHOME_DIR/bin/mnowatch.sh -super
- else
-  echo $btime","$remainder","`date -u` > /tmp/Not_a_voting_deadline
- fi
+test -n "$THIS_IS_CRON"||{ echo "This bash should be executed only by cron daemon.";exit 1;}
+block=$(dash-cli getblockcount)||{ echo "dashd error getting block height.";exit 2;}
+remainder=$((($block-880648+1662) % 16616))
+run_prog="$MYHOME_DIR/bin/mnowatch.sh"
+test -x "$run_prog"||{ echo "Cannot execute $run_prog";exit 3;}
+msg="$block,$remainder,$(date -u)"
+
+# We want to run mnowatch just after the voting has closed.
+if [ $remainder -le 10 ];then
+	echo "$msg" > /tmp/The_voting_deadline
+	"$run_prog" -super
+	exit 0
 else
-echo $(($(($(dash-cli getblockcount)-880648+1662)) % 16616))
- echo "This bash should be executed only by cron daemon."
+	echo "$msg" > /tmp/Not_a_voting_deadline
 fi
+
+# We want to run mnowatch at regular intervals the day before voting closes to catch last minute vote changes.
+# DASH block time is 2.625 mins on average, 60*24 mins in a day, 16616 blocks between cycles.
+# Thus, 16616 - (24*60/2.65) = 16068.  We run more often if the remainder is larger than this number.
+
+procs=$(ps aux)
+num_instances_this=$(echo "$procs"|grep "$0"|wc -l)
+num_instances_prog=$(echo "$procs"|grep "$run_prog"|wc -l)
+
+if [ $num_instances_this -le 1 -a $num_instances_prog -eq 0 -a $remainder -ge 16068 ];then
+	"$run_prog"
+	sleep 3600
+fi
+
+
