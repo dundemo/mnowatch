ee2036be0dab764221d05b4b44ac2ed82ee2f9ac
dundemo <demo@copernicus5>
2019-02-04 06:20:54 +1100

    btime.sh

--- btime.sh
+++ btime.sh
@@ -2,7 +2,7 @@
 #set -x
 
 # Licence: GPLv2
-# Tweaking / Debugging / Blockchain Advices by xkcd#7307@dashtalk Discord
+# Coder / Tweaking / Debugging / Blockchain Advices by xkcd#7307@dashtalk Discord
 #
 # MNOWATCH VERSION: 0.05
 
