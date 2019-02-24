86a8886cab2cc73c879f6454a6c0922510967ecd
dundemo <demo@copernicus5>
2019-02-25 08:47:16 +1100

    similarities html

--- ssdeepit.sh
+++ ssdeepit.sh
@@ -53,6 +53,7 @@ no=" "`echo $fn |cut -f2 -d","|sed -e s/":"/" "/g`" "
 abs=" "`echo $fn |cut -f3 -d","|sed -e s/":"/" "/g`" "
 voteshash=" "`echo $fn |cut -f4 -d","`" "
 IPS=" "`grep $voteshash mysorted.csv|cut -f1 -d","|sort`" "
+#Note: theIPSgrouphash contains " " in front and in the end
 theIPSgrouphash=`bc <<<ibase=16\;$(sha1sum <<<$IPS|tr a-z A-Z)0`
 theMNS=" "`grep $voteshash mysorted.csv|cut -f2 -d","`" "
 theMNSnum=" "`grep $voteshash mysorted.csv|cut -f2 -d","|wc -l`" "
@@ -129,64 +130,91 @@ cat /dev/null > used.txt
 if [ $SEARCHSIMILARITIES -gt 0 ]
 then
 
-for fn in `cat  mysortedUnique.csv`; do
-echo $fn|cut -f1-3 -d"," > this
+cat mysortedUnique.csv > mysortedUnique_gn.csv
+
+for fn in `cat mysortedUnique.csv`; do
+ echo $fn|cut -f1-3 -d"," > this
 #maybe I should use the recursive mode ssdeep -r , for performance reasons
-ssdeep -s this > this.db
-found=0
-IPS=0
-voteshashisfn=`echo $fn|cut -f4 -d","`
-isitusedfn=`grep $voteshashisfn used.txt |wc -l`
-ALLIPS=""
-ALLIPSgrouped=""
-if [ $isitusedfn -eq 0 ] 
- then
- for gn in `cat  mysortedUnique.csv`; do
-  isit=`echo $gn|cut -f1-3 -d"," | ssdeep -m this.db`
-  isitwc=`echo $isit|wc -c`
-  voteshashis=`echo $gn|cut -f4 -d","`
-  isitused=`grep $voteshashis used.txt |wc -l`
-  if [ $isitused -eq 0 ] 
+ ssdeep -s this > this.db
+ found=0
+ IPS=0
+ voteshashisfn=`echo $fn|cut -f4 -d","`
+ isitusedfn=`grep $voteshashisfn used.txt |wc -l`
+ ALLIPS=""
+ ALLIPSgrouped=""
+
+ grep -v $fn mysortedUnique_gn.csv > mysortedUnique_gntwo.csv
+
+ if [ $isitusedfn -eq 0 ] 
   then
-   if [ $isitwc -gt 1 ] 
+
+  for gn in `cat mysortedUnique_gntwo.csv`; do
+   isit=`echo $gn|cut -f1-3 -d"," | ssdeep -m this.db`
+   isitwc=`echo $isit|wc -c`
+   voteshashis=`echo $gn|cut -f4 -d","`
+   isitused=`grep $voteshashis used.txt |wc -l`
+   if [ $isitused -eq 0 ] 
    then
-   cutit=`echo $isit|cut -f2 -d"("|cut -f1 -d")"`
-   if [ $cutit -ne 100 ]
+    if [ $isitwc -gt 1 ] 
     then
-     if [ $cutit -gt $secondarg ]
-      then
-       ALLIPSofthisgroup=`grep $voteshashis $1|cut -f1 -d","`
-       #echo "Similarity="$cutit" voteshash="$voteshashis" IPs="$ALLIPSofthisgroup
-       ALLIPS=$ALLIPS" "$ALLIPSofthisgroup
-       ALLIPSgrouped=$ALLIPSgrouped"("$cutit":"$voteshashis":"$ALLIPSofthisgroup")"
-       echo $voteshashis >> used.txt
-       found=`expr $found + 1`
-	   IPSnum=`grep $voteshashis $1|cut -f1 -d","|wc -l`	
-       IPS=`expr $IPS + $IPSnum`
+    cutit=`echo $isit|cut -f2 -d"("|cut -f1 -d")"`
+    if [ $cutit -ne 100 ]
+     then
+      if [ $cutit -gt $secondarg ]
+       then
+        ALLIPSofthisgroup=`grep $voteshashis $1|cut -f1 -d","|sort`
+        ALLIPSofthisgroup2=" "`grep $voteshashis $1|cut -f1 -d","|sort`" "
+        #Note: theIPSgrouphashsimilar contains " " in front and in the end due to compatibility to uniquehashes
+        theIPSgrouphashsimilar=`bc <<<ibase=16\;$(sha1sum <<<$ALLIPSofthisgroup2|tr a-z A-Z)0`
+        ALLIPS=$ALLIPS" "$ALLIPSofthisgroup
+        ALLIPSgrouped=$ALLIPSgrouped"("$cutit":"$voteshashis":"$ALLIPSofthisgroup":"\"$theIPSgrouphashsimilar\"")"
+        echo $voteshashis >> used.txt
+        found=`expr $found + 1`
+        IPSnum=`grep $voteshashis $1|cut -f1 -d","|wc -l`	
+        IPS=`expr $IPS + $IPSnum`
+       fi
       fi
-     fi
+    fi
    fi
+  done
+ fi
+
+ cat mysortedUnique_gntwo.csv > mysortedUnique_gn.csv
+
+ if [ $found -gt 0 ]
+ then
+  ALLIPSofthisgroupfn=`grep $voteshashisfn $1|cut -f1 -d","|sort`
+  ALLIPSofthisgroupfn2=" "`grep $voteshashisfn $1|cut -f1 -d","|sort`" "
+#Note: theIPSgrouphashsimilarfn contains " " in front and in the end due to compatibility to uniquehashes
+  theIPSgrouphashsimilarfn=`bc <<<ibase=16\;$(sha1sum <<<$ALLIPSofthisgroupfn2|tr a-z A-Z)0`
+  ALLIPS=$ALLIPS" "$ALLIPSofthisgroupfn
+  ALLIPSgrouped=$ALLIPSgrouped"(100:"$voteshashisfn":"$ALLIPSofthisgroupfn":"\"$theIPSgrouphashsimilarfn\"")"
+  IPSnum=`grep $voteshashisfn $1|cut -f1 -d","|wc -l`	
+
+
+  IPS=`expr $IPS + $IPSnum`
+  ALLIPSsorted=`echo $ALLIPS|sed -s s/" "/\\\\n/g|sort -n`
+#Note: ALLIPShashis DOES NOT contains " " in front and in the end
+  ALLIPShashis=`bc <<<ibase=16\;$(sha1sum <<<$ALLIPSsorted|tr a-z A-Z)0`
+
+
+  echo $IPS",\""$ALLIPShashis"\","$ALLIPSsorted","$ALLIPSgrouped >> $filenameis
+
+  PREVIUSREPORTSIMILAR=`cd $HTTPD_DIR;ls -tra *similar*.html 2>/dev/null|tail -1`
+  PREVIUSREPORTSIMILARFULL=$HTTPD_DIR"/"$PREVIUSREPORTSIMILAR
+  existsfn=`grep $ALLIPShashis $PREVIUSREPORTSIMILARFULL 2>/dev/null|wc -l`
+  theHistoryfn=`grep -l $ALLIPShashis $HTTPD_DIR/*similar*.html 2>/dev/null|wc -l`
+  theHistoryfn=`printf %04d $theHistoryfn`
+
+  if [ $existsfn -eq 0 ]
+  then
+  echo "<tr id=\""$ALLIPShashis"\"><td><div>"$IPS"</div></td><td><div>(History="$theHistoryfn") <strong>"$ALLIPShashis"</strong></div></td><td><div>"$ALLIPSsorted"</div></td><td><div>"$ALLIPSgrouped"</div></td></tr>" >> $filenameishtml
+  else
+  echo "<tr id=\""$ALLIPShashis"\"><td><div>"$IPS"</div></td><td><div>(History="$theHistoryfn") <a href=\"./"$PREVIUSREPORTSIMILAR"#"$ALLIPShashis"\">"$ALLIPShashis"</a></div></td><td><div>"$ALLIPSsorted"</div></td><td><div>"$ALLIPSgrouped"</div></td></tr>" >> $filenameishtml
   fi
- done
-fi
-if [ $found -gt 0 ]
-then
- ALLIPSofthisgroupfn=`grep $voteshashisfn $1|cut -f1 -d","`
- #echo "The above "$found" voteshash(es) is(are) similar to the "$voteshashisfn" IPs="$ALLIPSofthisgroupfn
- ALLIPS=$ALLIPS" "$ALLIPSofthisgroupfn
- ALLIPSgrouped=$ALLIPSgrouped"(100:"$voteshashisfn":"$ALLIPSofthisgroupfn")"
- IPSnum=`grep $voteshashisfn $1|cut -f1 -d","|wc -l`	
- IPS=`expr $IPS + $IPSnum`
- ALLIPSsorted=`echo $ALLIPS|sed -s s/" "/\\\\n/g|sort -n`
- ALLIPShashis=`bc <<<ibase=16\;$(sha1sum <<<$ALLIPSsorted|tr a-z A-Z)0`
- #echo "GROUP's hash="$ALLIPShashis 
- #echo "ALL IPs sorted="`echo $ALLIPSsorted`
- #echo "TOTAL MASTERNODES OF THE GROUP="$IPS
- #echo "ALL IPs of the group="$ALLIPSgrouped
- echo $IPS",\""$ALLIPShashis"\","$ALLIPSsorted","$ALLIPSgrouped >> $filenameis
- echo "<tr><td><div>"$IPS"</div></td><td><div>"$ALLIPShashis"</div></td><td><div>"$ALLIPSsorted"</div></td><td><div>"$ALLIPSgrouped"</div></td></tr>" >> $filenameishtml
- echo $voteshashisfn >> used.txt
-fi
+
+  echo $voteshashisfn >> used.txt
+ fi
 done
 
 rm this
@@ -211,6 +239,10 @@ echo "</tbody></table></body></html>" >> $filenameishtml
 
 rm tmpsort
 rm mysortedUnique.csv
+
+rm mysortedUnique_gntwo.csv 
+rm mysortedUnique_gn.csv
+
 rm mysorted.csv
 rm used.txt
 rm pasted.html
